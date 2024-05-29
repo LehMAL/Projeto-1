a498f5e18956e8e905571201c077a5c63e561cb3
Lêh <lczrleh@gmail.com>
2024-05-29 16:28:22 -0300

    bd

--- app-teste/src/Pages/HomePage.js
+++ app-teste/src/Pages/HomePage.js
@@ -1,36 +1,90 @@
-import bela_fera_capa from '../Images/poster_bela_fera.jpeg';
-import black_phone_capa from '../Images/poster_black_phone.jpg';
-import it_capa from '../Images/poster_it.jpg';
-import malevola_capa from '../Images/poster_malevola.jpg';
-import morte_capa from '../Images/poster_morte_parabens.jpg';
-import fabrica_capa from '../Images/poster_fabrica_chocolate.jpg';
-import rango_capa from '../Images/poster_rango.jpg';
-import peculiares_capa from '../Images/poster_peculiares.jpg';
-import paranormal_capa from '../Images/poster_paranorman.jpeg';
-import piratas_capa from '../Images/poster_piratas_pirados.jpeg';
-import festa_capa from '../Images/poster_festa_ceu.jpeg';
-import tesoura_capa from '../Images/poster_tesoura.jpg';
-import panico_capa from '../Images/poster_panico.jpg';
-import nove_capa from '../Images/poster_nove.jpg';
-import oz_capa from '../Images/poster_oz.jpg';
-import gato_preto from '../Images/livro_gato_preto.jpg';
-import insurgente from '../Images/livro_insurgente.jpg';
-import it from '../Images/livro_it.jpg';
-import pijama from '../Images/livro_pijama.jpg';
-import vbs from '../Images/livro_vbs.jpg';
-import divergente from '../Images/livro_divergente.jpg';
-import hp_enigma from '../Images/livro_hp_enigma.jpg';
-import hp_reliquias from '../Images/livro_hp_reliquias.jpg';
-import pj from '../Images/livro_pj.jpg';
-import orgulho from '../Images/livro_orgulho.jpg';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import {  Row, Col, Card } from 'react-bootstrap';
+import Slider from "react-slick";
+import '../Components/ImageSliderComponent.css';
+
+
 
 function HomePage(){
+
+    const [filmes, setFilmes] = useState([]);
+    const [livros, setLivros] = useState([]);
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        appendDots: dots => (
+          <div
+            style={{
+              padding: "30px"
+            }}
+          >
+            <ul style={{ margin: "0px" }}> {dots} </ul>
+          </div>
+        ),
+        customPaging: i => (
+          <div
+            style={{
+              color: "black",
+              borderRadius: "10px",
+              border: "5px black solid"
+            }}
+          >
+          </div>
+        )
+      };
+   
+
+    //realizar ações ao iniciar componente
+    useEffect(() => {
+        async function buscarFilmes() {
+            const filmes = axios.get("http://143.198.156.185/api/home").then(function (value) {
+                setFilmes(value.data);
+            }).catch(function (value) {
+                console.log(value);
+            });
+        }
+
+        async function buscaLivros() {
+            const filmes = axios.get("http://143.198.156.185/api/home").then(function (value) {
+                setLivros(value.data);
+            }).catch(function (value) {
+                console.log(value);
+            });
+        }
+        buscarFilmes();
+        buscaLivros();
+    });
 
 
     return(
         <div className="pt-2">
 
             <div className="card mb-5 mx-3">
+            <div className="slider-container">
+
+            <Slider {...settings}>
+                    {filmes.map((filme, index) => (
+                        <div className="slide" key={index}>
+                            <img
+                                className="d-block w-100"
+                                src={filme.imagens[2]}
+                                alt={filme.titulo}
+                            />
+                            <div className="slide">
+                                <h3>{filme.title}</h3>
+                                <p>{filme.sinopse}</p>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+                </div>
 
                 <div className="card-header mx-3 mt-3 borda" >                        
                 <h2>Livros</h2>
@@ -44,27 +98,18 @@ function HomePage(){
 
                             <div className="catalog-grid ">
 
-                        
-                            <img src={gato_preto} title=' O Gato Preto' className="catalog-image" />
-
-                            <img src={it} title='it a Coisa' className="catalog-image" />
-
-                            <img src={pijama} title='O menino do Pijama Listrado' className="catalog-image" />
-
-                            <img src={vbs} title='Vermelho Branco Sangue Azul' className="catalog-image" />
-
-                            <img src={hp_enigma} title='Harry Potter e o enigma do príncipe' className="catalog-image" />
-
-                            <img src={insurgente} title='Insurgente' className="catalog-image" />
-
-                            <img src={divergente} title='Divergente' className="catalog-image" />
-
-                            <img src={hp_reliquias} title='Harry Potter e as relíquias da morte' className="catalog-image" />
-
-                            <img src={pj} title='Percy Jackson' className="catalog-image" />
-
-                            <img src={orgulho} title='Orgulho e Preconceito' className="catalog-image" />
-                        </div>
+                            <Row className="pt-2 pb-3">
+                            {livros.map((livro, index) => (
+                            <Col xs={6} sm={4} md={3} lg={2}>
+                            <Card>
+                            <Card.Img variant="top" src={livro.url_thumbnail} />
+                            <Card.Body>
+                                <Card.Title>{livro.titulo}</Card.Title>
+                            </Card.Body>
+                            </Card>
+                            </Col>
+                            ))}
+                            </Row>
                         </div>
 
                            
@@ -75,41 +120,24 @@ function HomePage(){
                       <div className="catalog-grid ">
                
 
-                            <img src={bela_fera_capa} title='A Bela e a Fera' className="catalog-image" />
-
-                            <img src={black_phone_capa} title='Telefone Preto' className="catalog-image" />
-
-                            <img src={fabrica_capa} title='A fantastica fabrica de chocolate' className="catalog-image" />
-
-                            <img src={it_capa} title='It a Coisa' className="catalog-image" />
-
-                            <img src={paranormal_capa} title='Paranorman' className="catalog-image" />
-
-                            <img src={malevola_capa} title='Malévola' className="catalog-image" />
-
-                            <img src={morte_capa} title='A Morte te da Parabéns' className="catalog-image" />
-
-                            <img src={piratas_capa} title='Piratas Pirados' className="catalog-image" />
-
-                            <img src={peculiares_capa} title='O lar das crianças Peculiares' className="catalog-image" />
-
-                            <img src={rango_capa} title='Rango' className="catalog-image" />
-
-                            <img src={tesoura_capa} title='Edward mãos de tesoura' className="catalog-image" />
-
-                            <img src={festa_capa} title='Viva a vida é uma festa' className="catalog-image" />
-
-                            <img src={panico_capa} title='Panico' className="catalog-image" />
-
-                            <img src={nove_capa} title='Nove em busca da salvação' className="catalog-image" />
-
-                            <img src={oz_capa} title='O mágico de Oz' className="catalog-image" />
-
+                        <Row className="pt-2 pb-3">
+                        {filmes.map((filme, index) => (
+                        <Col xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <Card.Img variant="top" src={filme.url_thumbnail} />
+                            <Card.Body>
+                                <Card.Title>{filme.titulo}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                         ))}
+                        </Row>
 
                             </div>
                         </div>
                     </div>
             </div>
+    </div>
     </div>
 
 
